6e805aa107bc1279a93bd2eb46aafea5c7f5193a
Aruna Silva <aruna@intervest.lk>
2022-12-12 07:54:50 +0530

    HashMap behavior test

---
+++ src/main/java/com/learn/example/corejava/HashMapTest.java
@@ -0,0 +1,50 @@
+package com.learn.example.corejava;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+
+        Map<Person, Integer> personMap = new HashMap();
+        personMap.put(new Person("Aruna", 30), 30);
+
+        Integer age = personMap.get(new Person("Aruna", 30));
+        System.out.println("Age is : " + age.intValue());
+
+
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
