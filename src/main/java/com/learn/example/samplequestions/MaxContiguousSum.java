1f76db57aaafd3c4edd73f5c437814d53c8b830d
Aruna Silva <arunasilva86@gmail.com>
2023-02-20 11:33:18 +0530

    minor bug fix

--- src/main/java/com/learn/example/samplequestions/MaxContiguousSum.java
+++ src/main/java/com/learn/example/samplequestions/MaxContiguousSum.java
@@ -5,12 +5,12 @@ import java.util.List;
 /*
  * Find the max sum of the n consecutive values of a given array where n can be any integer value less than or equals a given integer k
  *
- * Ex 1 : {5, -2, 15, -4, 6, 1, } and k = 3 then max sum is 18, as (5 + (-2) + 15) = 18 // max gives at n=3, (=k)
+ * Ex 1 : {5, -2, 15, -4, 9, 1} and k = 3 then max sum is 20, as (15 + (-4) + 9) = 20 // max gives at n=3, (=k)
  * Ex 2 : {-1, -1, -1, 200, 300, -1, -1} and k = 4 then max sum is 500, as (200 + 300) = 500 // max gives at n=2, (<k)
  */
 public class MaxContiguousSum {
     public static void main(String[] args) {
-        List<Integer> integerList = List.of(5, -2, 15, -4, 6, 1, 9);
+        List<Integer> integerList = List.of(5, -2, 15, -4, 9, 1);
         System.out.println("Returned is : " + doSomething(integerList, 3));
     }
 
@@ -23,7 +23,7 @@ public class MaxContiguousSum {
             long currentSum = 0;
             int startIndex = 0;
             int endIndex = 0;
-            for (int index = 0; index < pnl.size() - count; index++) {
+            for (int index = 0; index < pnl.size() - count + 1; index++) {
                 startIndex = index;
                 endIndex = index + count - 1;
                 if (startIndex == 0) {
