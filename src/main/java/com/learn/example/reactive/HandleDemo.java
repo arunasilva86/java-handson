5068c7feca90bd888e36aea8e80be898f827bfda
Aruna Silva <arunasilva86@gmail.com>
2023-05-06 15:16:47 +0530

    another hamdle method example added

--- src/main/java/com/learn/example/reactive/HandleDemo.java
+++ src/main/java/com/learn/example/reactive/HandleDemo.java
@@ -1,6 +1,8 @@
 package com.learn.example.reactive;
 
 import com.learn.example.reactive.common.DefaultSubscriber;
+import net.datafaker.Faker;
+import net.datafaker.sequence.FakeCollection;
 import reactor.core.publisher.Flux;
 
 public class HandleDemo {
@@ -12,12 +14,28 @@ public class HandleDemo {
                     // Emit integer when integer > 30 and complete when > 70
                     if (integer > 70) {
                         synchronousSink.complete();
-                    }
-                    if (integer > 30) {
+                    } else if (integer > 30) {
                         synchronousSink.next(integer);
                     }
                 });
 
         flux.subscribe(new DefaultSubscriber());
+
+        System.out.println("------------------canada example with handle");
+
+
+        Faker faker = new Faker();
+        Flux  flux2 = Flux.generate(synchronousSink -> synchronousSink.next(faker.country().name()))
+                .map(Object::toString)
+                .handle((s, synchronousSink) -> {
+                    synchronousSink.next(s);
+                    if ("Canada".equals(s)) {
+                        synchronousSink.complete();
+                    }
+                });
+        flux2.subscribe(new DefaultSubscriber());
+
     }
+
+
 }
