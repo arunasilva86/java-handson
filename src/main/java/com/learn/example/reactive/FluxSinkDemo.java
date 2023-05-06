b9b18de0f6b8e9007c86f44e4b3177c834ef84f9
Aruna Silva <arunasilva86@gmail.com>
2023-05-06 10:28:59 +0530

    Flux.create deme added

---
+++ src/main/java/com/learn/example/reactive/FluxSinkDemo.java
@@ -0,0 +1,36 @@
+package com.learn.example.reactive;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+import java.util.function.Consumer;
+
+public class FluxSinkDemo {
+
+    private static NameProducer nameProducer = new NameProducer();
+
+    public static void main(String[] args) {
+        Flux.create(nameProducer)
+                .subscribe(s -> System.out.println("Sub" + s), throwable -> System.out.println(throwable.getMessage()), () -> System.out.println("COMPLETED....."));
+
+        // somewhere else in the code flow
+        nameProducer.produceData();
+    }
+}
+
+class NameProducer implements Consumer<FluxSink<String>> {
+
+    FluxSink<String> stringFluxSink;
+
+    @Override
+    public void accept(FluxSink<String> stringFluxSink) {
+        this.stringFluxSink = stringFluxSink;
+    }
+
+    public void produceData () {
+        for (int i = 0; i < 10; i++ ) {
+            stringFluxSink.next("Emitted  : " + i);
+        }
+
+    }
+}
