eeeb29bf02afc9826bd4bcfd579d4339bb90ab9a
Aruna Silva <arunasilva86@gmail.com>
2023-05-06 10:28:15 +0530

    Added a defauilt Subscriber

---
+++ src/main/java/com/learn/example/reactive/common/DefaultSubscriber.java
@@ -0,0 +1,30 @@
+package com.learn.example.reactive.common;
+
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+public class DefaultSubscriber implements Subscriber<Object> {
+
+    @Override
+    public void onSubscribe(Subscription s) {
+        s.request(Long.MAX_VALUE);
+    }
+
+    @Override
+    public void onNext(Object o) {
+        System.out.println(" Received : " + o);
+    }
+
+
+    @Override
+    public void onError(Throwable t) {
+        System.out.println("ERROR Received : " + t.getMessage());
+
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println("Received COMPLETED");
+    }
+}
