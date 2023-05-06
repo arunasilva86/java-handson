f0664d081ea52387c7d0afeecaef6847e7a9c444
Aruna Silva <arunasilva86@gmail.com>
2023-05-06 21:58:57 +0530

    limitrate demo

--- src/main/java/com/learn/example/reactive/LimitRateDemo.java
+++ src/main/java/com/learn/example/reactive/LimitRateDemo.java
@@ -1,4 +1,14 @@
 package com.learn.example.reactive;
 
+import com.learn.example.reactive.common.DefaultSubscriber;
+import reactor.core.publisher.Flux;
+
 public class LimitRateDemo {
+    public static void main(String[] args) {
+        Flux.range(1, 100)
+                .log()
+                .limitRate(10, 80) // There will be a request(8) after each eight set consumed by downstream
+                .log()
+                .subscribe(new DefaultSubscriber());  // initial request(unbounded) call, only once
+    }
 }
