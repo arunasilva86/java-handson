20ca10eaa919e0a0fbee107dbe69c9cae68d78d5
Aruna Silva <arunasilva86@gmail.com>
2023-05-06 21:52:08 +0530

    added more do methods

---
+++ src/main/java/com/learn/example/reactive/DoOperatorsDemo.java
@@ -0,0 +1,28 @@
+package com.learn.example.reactive;
+
+import com.learn.example.reactive.common.DefaultSubscriber;
+import reactor.core.publisher.Flux;
+
+public class DoOperatorsDemo {
+
+    public static void main(String[] args) {
+        Flux.create(fluxSink -> {
+            System.out.println("Inside create ... ");
+            for (int x = 1; x <= 5; x++) {
+                fluxSink.next(x);
+            }
+            fluxSink.complete();
+
+        })
+                .doOnNext(o -> System.out.println("doOnNext-1"))
+                .doOnNext(o -> System.out.println("doOnNext-2"))
+                .doOnComplete(() -> System.out.println("doOnComplete-1"))
+                .doOnComplete(() -> System.out.println("doOnComplete-2"))
+                .doOnRequest(value -> System.out.println("doOnRequest-1"))
+                .doOnRequest(value -> System.out.println("doOnRequest-2"))
+                .doOnSubscribe(o -> System.out.println("doOnSubscribe-1"))
+                .doOnSubscribe(o -> System.out.println("doOnSubscribe-2"))
+        .subscribe(new DefaultSubscriber());
+
+    }
+}
