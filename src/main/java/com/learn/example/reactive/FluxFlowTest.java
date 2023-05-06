a94a0170bd8679c054534b45f13a949359b2ac12
Aruna Silva <arunasilva86@gmail.com>
2023-05-06 10:27:11 +0530

    renamed the file FluxTest.java as FluxFlowTest.java

--- src/main/java/com/learn/example/reactive/FluxFlowTest.java
+++ src/main/java/com/learn/example/reactive/FluxFlowTest.java
@@ -5,7 +5,25 @@ import reactor.core.publisher.Flux;
 
 import java.util.LinkedList;
 
-public class FluxTest {
+public class FluxFlowTest {
+
+    public static void main(String[] args) {
+        Flux<String> stringFlux =
+        /* 1 */        Flux.range(1, 10)
+                            .log()
+        /* 2 */             .filter(integer -> integer % 2  == 0)
+                            .log()
+        /* 3 */             .map(integer -> "hi all...")
+                            .log()
+        /* 4 */             .map(String::toUpperCase);
+
+        stringFlux.log()
+        /* 5 */             .map(s -> s + "XX")
+                            .log()
+        /* 6 */             .filter(s  -> s.startsWith("d"))
+                            .log()
+                            .subscribe(System.out::println);
+    }
 
 //    @Test
     public void testBasic () {
