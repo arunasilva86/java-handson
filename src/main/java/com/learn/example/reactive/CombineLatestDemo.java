3e9036e44c10088c218e135f6632aad15c4018dd
Aruna Silva <arunasilva86@gmail.com>
2023-05-21 12:46:40 +0530

    CombineLatestDemo

--- src/main/java/com/learn/example/reactive/CombineLatestDemo.java
+++ src/main/java/com/learn/example/reactive/CombineLatestDemo.java
@@ -3,28 +3,32 @@ package com.learn.example.reactive;
 import com.learn.example.reactive.common.DefaultSubscriber;
 import com.learn.example.reactive.common.Util;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 import reactor.util.function.Tuple3;
 
-public class ZipDemo {
+import java.time.Duration;
+
+public class CombineLatestDemo {
 
     public static void main(String[] args) {
-        Flux<Tuple3> zippedFlux = Flux.zip(getMonitor(), getCpu(), getKeyBoard()); // emit onNext once received an object from all the subscribers
+        Flux.combineLatest(getMonitor(), getCpu(), (o, o2) ->  o + " / " + o2)
 
-        zippedFlux
-                .doOnNext(o -> System.out.println("doOnNext " + " Monitor-" + o.getT1() + " CPU-" + o.getT2() + " KeyBoard-" + o.getT3()))
+                .subscribeOn(Schedulers.boundedElastic())
                 .subscribe(new DefaultSubscriber());
+
+        Util.sleepSec(20);
     }
 
-    private static Flux getMonitor() {
+    private static Flux<String> getMonitor() {
         return Flux.range(1, 5)
                 .map(integer -> "Monitor " + integer)
-                .doOnNext(s -> Util.sleepSec(1));
+                .delayElements(Duration.ofSeconds(1));
     }
 
-    private static Flux getCpu() {
-        return Flux.range(1, 3)
+    private static Flux<String>  getCpu() {
+        return Flux.range(1, 6)
                 .map(integer -> "CPU " + integer)
-                .doOnNext(s -> Util.sleepSec(2));
+                .delayElements(Duration.ofSeconds(3));
     }
 
     private static Flux getKeyBoard() {
