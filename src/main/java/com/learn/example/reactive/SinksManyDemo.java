2c150dec40c4c41a4428b9ee1f31d7c0f4fa293b
Aruna Silva <arunasilva86@gmail.com>
2023-06-07 01:25:54 +0530

    Sinks many added

---
+++ src/main/java/com/learn/example/reactive/SinksManyDemo.java
@@ -0,0 +1,35 @@
+package com.learn.example.reactive;
+
+import com.learn.example.reactive.common.DefaultSubscriber;
+import com.learn.example.reactive.common.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+import reactor.core.scheduler.Schedulers;
+
+public class SinksManyDemo {
+    public static void main(String[] args) {
+        Sinks.Many<Integer> intSinks = Sinks.many().multicast().onBackpressureBuffer();
+
+        intSinks.tryEmitNext(10);
+        intSinks.tryEmitNext(20);
+
+        Flux<Integer> integerFlux =  intSinks.asFlux();
+
+        integerFlux
+                .doOnNext(integer -> Util.printThreadName(integer + " Aruna "))
+                .subscribe(new DefaultSubscriber("Aruna"));
+
+        intSinks.tryEmitNext(30);
+
+        integerFlux
+                .publishOn(Schedulers.boundedElastic()) // Each subscriber can decide processing thread model of it
+                .doOnNext(integer -> Util.printThreadName(integer + " Ishara "))
+                .subscribe(new DefaultSubscriber("Ishara"));
+
+        intSinks.tryEmitNext(40); // Ishara eceive only 40, 50 60. While Aruna receive all
+        intSinks.tryEmitNext(50);
+        intSinks.tryEmitNext(60);
+
+        Util.sleepSec(10);
+    }
+}
--- src/main/java/com/learn/example/reactive/SinksOneDemo.java
+++ src/main/java/com/learn/example/reactive/SinksOneDemo.java
@@ -5,19 +5,19 @@ import com.learn.example.reactive.common.Util;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
-public class SinksDemo {
+public class SinksOneDemo {
 
     public static void main(String[] args) {
 
         Sinks.One<Integer> sink = Sinks.one();
 
-
-
         sink.asMono()
+                .doOnNext(integer -> Util.printThreadName(integer))
                 .subscribe(new DefaultSubscriber("Aruna"));
 
-        System.out.println("before emit");
+        Util.printThreadName("before emit : ");
         sink.tryEmitValue(150);
+        Util.printThreadName("after emit : ");
 //        Util.sleepSec(10);
 
     }
